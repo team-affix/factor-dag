f5b88462f2272b3d17fca40cb6bbff5cf6782663
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2024-04-16 00:03:53 -0700

    Added basic unit tests

--- main.cpp
+++ main.cpp
@@ -69,6 +69,70 @@ void test_make_coverage(
 
 }
 
+void test_literal_sign(
+
+)
+{
+    assert(sign(literal(0)) == 0);
+    assert(sign(literal(1)) == 1);
+    assert(sign(literal(2)) == 0);
+    assert(sign(literal(3)) == 1);
+    assert(sign(literal(47)) == 1);
+    assert(sign(literal(48)) == 0);
+}
+
+void test_literal_index(
+
+)
+{
+    assert(index(literal(0)) == 0);
+    assert(index(literal(1)) == 0);
+    assert(index(literal(2)) == 1);
+    assert(index(literal(3)) == 1);
+    assert(index(literal(47)) == 23);
+    assert(index(literal(48)) == 24);
+}
+
+void test_literal_covers(
+
+)
+{
+    input l_input = { 0, 1, 1, 0, 1, 0 };
+
+    assert(covers(literal(0), &l_input) == true);
+    assert(covers(literal(1), &l_input) == false);
+    assert(covers(literal(2), &l_input) == false);
+    assert(covers(literal(3), &l_input) == true);
+    assert(covers(literal(4), &l_input) == false);
+    assert(covers(literal(5), &l_input) == true);
+    assert(covers(literal(6), &l_input) == true);
+    assert(covers(literal(7), &l_input) == false);
+    assert(covers(literal(8), &l_input) == false);
+    assert(covers(literal(9), &l_input) == true);
+    assert(covers(literal(10), &l_input) == true);
+    assert(covers(literal(11), &l_input) == false);
+    
+}
+
+void test_make_literals(
+
+)
+{
+    constexpr size_t VAR_COUNT = 10;
+    
+    std::set<literal> l_literals = make_literals(VAR_COUNT);
+
+    /// The number of literals generated should
+    ///     always be twice the number of variables.
+    assert(l_literals.size() == 2 * VAR_COUNT);
+
+    /// Ensure that all expected literals exist
+    ///     within the resultant set.
+    for (literal i = 0; i < l_literals.size(); i++)
+        assert(l_literals.contains(i));
+    
+}
+
 void test_small_generalization_0(
 
 )
@@ -160,6 +224,10 @@ void unit_test_main(
 
     TEST(test_input_construction);
     TEST(test_make_coverage);
+    TEST(test_literal_index);
+    TEST(test_literal_sign);
+    TEST(test_literal_covers);
+    TEST(test_make_literals);
     TEST(test_small_generalization_0);
     TEST(test_small_generalization_1);
     
